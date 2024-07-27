32f353e55bdc10f31bf0875c3cad927e7fd407c1
Bhadresh Desai <bhadresh_desai@hotmail.com>
2024-07-27 12:13:16 +0100

    first commit

---
+++ plugins/omp/omp.plugin.zsh
@@ -0,0 +1,5 @@
+#OMP_THEMES=$HOME/work/opensource/oh-my-posh/themes
+#eval "$(oh-my-posh init zsh --config $OMP_THEMES/tokyonight_storm.omp.json)"
+# eval "$(oh-my-posh init zsh --config $OMP_THEMES/atomic.omp.json)"
+OMP_THEMES=$HOME/.oh-my-zsh/custom/plugins/omp
+eval "$(oh-my-posh init zsh --config $OMP_THEMES/tokyonight_storm.omp.json)"
