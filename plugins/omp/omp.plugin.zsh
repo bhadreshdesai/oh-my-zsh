8e03de929b94a082c2065d7d6c9859187651aed5
Bhadresh Desai <bhadresh_desai@hotmail.com>
2024-07-27 12:47:53 +0100

    move oh-my-zsh custom to work folder

--- plugins/omp/omp.plugin.zsh
+++ plugins/omp/omp.plugin.zsh
@@ -1,5 +1,5 @@
 #OMP_THEMES=$HOME/work/opensource/oh-my-posh/themes
 #eval "$(oh-my-posh init zsh --config $OMP_THEMES/tokyonight_storm.omp.json)"
 # eval "$(oh-my-posh init zsh --config $OMP_THEMES/atomic.omp.json)"
-OMP_THEMES=$HOME/.oh-my-zsh/custom/plugins/omp
+OMP_THEMES=$HOME/work/oh-my-zsh/plugins/omp
 eval "$(oh-my-posh init zsh --config $OMP_THEMES/tokyonight_storm.omp.json)"
