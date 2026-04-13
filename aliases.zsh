d6aca56a602be3fe52c6aa1c682fa9b63488614a
Bhadresh Desai <bhadresh_desai@hotmail.com>
2026-04-13 14:53:20 +0100

    add ij aias

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,12 @@
-alias jb='code ~/Clients/JB'
+alias bb-proxy='export ALL_PROXY=http://webproxy.infra.backbase.cloud:8888
+    export HTTP_PROXY=$ALL_PROXY
+    export HTTPS_PROXY=$ALL_PROXY
+    export FTP_PROXY=$ALL_PROXY
+    export RSYNC_PROXY=$ALL_PROXY
+    export http_proxy=$ALL_PROXY
+    export https_proxy=$ALL_PROXY
+    export ftp_proxy=$ALL_PROXY
+    export rsync_proxy=$ALL_PROXY'
 alias db='code ~/code/bhadresh_backbase/mssql-db/'
+alias ij='/Applications/IntelliJ IDEA CE.app/Contents/MacOS/idea'
+alias jb='code ~/Clients/JB'
